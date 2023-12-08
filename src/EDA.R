9e4ddac692ec02acaad7b41d52105d9595dd9710
DarioTrujanoOchoa <dtrujano.22@gmail.com>
2023-12-07 17:04:25 -0800

    EDA of the whole data

--- src/EDA.R
+++ src/EDA.R
@@ -4,17 +4,92 @@ library(pacman)
 p_load(tidyverse, 
        naniar)
 
+# load data ----
 notes <- read_tsv("data/notes-00000.tsv")
 spec(notes)
+
+status <- read_tsv("data/noteStatusHistory-00000.tsv")
+spec(status)
+
+r0 <- read_tsv("data/ratings-00000.tsv")
+r1 <- read_tsv("data/ratings-00001.tsv")
+r2 <- read_tsv("data/ratings-00002.tsv")
+r3 <- read_tsv("data/ratings-00003.tsv")
+
+spec(r0)
+
+# Almost all the notes in notes are in status
+# only 20 notes are not there
+sum(notes$noteId %in% status$noteId)
+
 s_notes <- slice_sample(notes,n = 10000) 
+s_status <- slice_sample(status,n = 10000) 
+vis_miss(s_notes)
+vis_miss(s_status)
+
+# notes ----
+
+## Authors ----
+# the observations in the dataset are unique for each note
+length(unique(notes$noteId))
+
+# the Some notes are made by more than one participant
+length(unique(s_notes$noteAuthorParticipantId))
+# most users publish a note only once (60% in sample) 
+table(table(s_notes$noteAuthorParticipantId)) 
+barplot(table(table(s_notes$noteAuthorParticipantId)),
+        xlab = "Number of Notes",
+        ylab = "Number of Authors",
+        main = "Distribution of Notes Published by Author")
+# ID of prolific authors
+head(sort(table(s_notes$noteAuthorParticipantId),decreasing = T),n = 25 )
+
+## Tweets ----
+# some tweets have more than one note
+# most of the tweets have a single note
+length(unique(notes$tweetId))
+
+barplot(table(table(s_notes$tweetId)),
+        xlab = "Number of Notes",
+        ylab = "Number of Tweets",
+        main = "Distribution of Notes Published by Tweet")
 
 # all the variables starting with misleading are answers to the reason something was misleading
+# all variables starting with notMisleadnig are answers to the reason something was not misleading
 # therefore cannot be used to explain why a note is misleading or not
 
-as_tibble(s_notes) %>% select_if(is.numeric) %>%
+as_tibble(s_notes) %>% 
+  filter(classification == "MISINFORMED_OR_POTENTIALLY_MISLEADING") %>% 
+  select_if(is.numeric) %>%
   map_dbl(sum) %>% as.data.frame()
 
-vis_miss(s_notes)
 
-table(s_notes$classification,s_notes$misleadingOther)
-table(s_notes$classification,s_notes$misleadingFactualError)
+
+table(s_notes$classification,s_notes$notMisleadingOther)
+table(s_notes$classification,s_notes$isMediaNote)
+
+table(s_notes$classification,s_notes$trustworthySources)
+
+# status ----
+
+# the observations in the dataset are unique
+length(unique(status$noteId))
+
+# Contains -1 if the note never left “Needs More Ratings” status.
+# most of the notes needed more ratings
+sum(status$timestampMillisOfFirstNonNMRStatus==-1)
+barplot(table(status$currentStatus))
+
+table(status$currentStatus, status$lockedStatus)
+
+
+# ratings ----
+
+# most of the notes are in the ratings dataset
+sum(r0$noteId %in% notes$noteId)
+sum(notes$noteId %in% r0$noteId)
+
+barplot(table(table(r0$noteId)),
+        xlab = "Number of Ratings",
+        ylab = "Number of Notes",
+        main = "Distribution of Ratings Published by Note")
