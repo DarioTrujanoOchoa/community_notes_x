7a0de1f92581f97e8a74b30a212887a94223bb8f
NateHurst <natehurstschool@gmail.com>
2025-01-28 23:07:18 -0800

    update EDA

--- src/eda.R
+++ src/eda.R
@@ -7,6 +7,7 @@ library(pacman)
 p_load(tidyverse,
        ggplot2,
        corrplot)
+rm(list=ls())
 
 load("data/notes_merged.RData")
 
@@ -15,7 +16,7 @@ load("data/notes_merged.RData")
 missing_cell <- which(is.na(notes_merged), arr.ind = TRUE)
 notes_merged[missing_cell[,1],]
 # These are the tweet ids
-tweet_ids <- notes_merged[missing_cell[,1],] %>% select(tweet_id) %>% pull() %>% format(scientific = F) %>% unique()
+tweet_ids_missing <- notes_merged[missing_cell[,1],] %>% select(tweet_id) %>% pull() %>% format(scientific = F) %>% unique()
 # There is no summary in this notes, probably this was a mistake
 # There is nothing in the note 1370110240532930560 that had 8 ratings. The other two notes were never rated.
 
@@ -83,3 +84,102 @@ notes_merged %>% group_by(current_status) %>%
             )
 
 table(notes_merged$classification, notes_merged$current_status)
+
+
+# Nate EDA ----
+# Remove scientific notation/filter for tweets with ratings
+notes_merged$tweet_id <- notes_merged$tweet_id %>% format(scientific = F)
+notes_merged <- notes_merged %>% filter(ratings>0)
+# Look at number of ratings to get a note published
+notes_merged %>% group_by(current_status) %>% 
+  summarise(quantile(ratings,probs = 0.25))
+
+# Extract published notes with low ratings
+notes_merged %>% 
+  filter(ratings < 6, current_status != "NEEDS_MORE_RATINGS", helpful_rate >= 0.75) %>% 
+  View()
+# Not all notes that have enough ratings are actually published
+
+# Grab 100 ids to use in python
+
+# Ggplot visualisations
+id_snippet <- notes_merged %>% select(tweet_id) %>% head(100)
+View(id_snippet)
+snippet_list <- paste0("[", paste(id_snippet$tweet_id, collapse = ","), "]")
+#write(snippet_list, file = "tweet_ids.txt")
+
+### Far more misleading ----
+ggplot(notes_merged, aes(x = classification)) +
+  geom_bar() +
+  labs(title = "Distribution of Classifications")
+
+### Far more needs more ratings ----
+ggplot(notes_merged, aes(x = current_status)) +
+  geom_bar() +
+  labs(title = "Distribution of Current Status")
+
+### Notes need high agreement to be published ----
+notes_merged %>% filter(agreement_rate>0) %>% 
+ggplot(aes(x = agreement_rate)) +
+  geom_histogram(binwidth = 0.05) +
+  labs(title = "Distribution of Agreement Rate")
+
+### Trustworthy sources = Misleading? ----
+ggplot(notes_merged, aes(x = classification, fill = factor(trustworthy_sources))) +
+  geom_bar(position = "dodge") +
+  labs(title = "Classification by Trustworthy Sources",
+       fill = "Trustworthy Sources")
+
+sampled_data <- notes_merged[sample(nrow(notes_merged), 10000), ]
+
+# Mega correlation plot
+library(GGally)
+ggpairs(sampled_data[, c("ratings", "agreement_rate", "helpful_rate", "not_helpful_rate", "somewhat_helpful_rate", "classification", "current_status")])
+
+# No observed correlation between classification and helpfulness
+ggplot(sampled_data, aes(x = helpful_rate, y = not_helpful_rate, color = classification)) +
+  geom_point() +
+  labs(title = "Helpful vs Not Helpful Rate by Classification")
+
+
+### Distribution showing when notes are published ----
+ggplot(notes_merged, aes(x = ratings, fill = current_status)) +
+  geom_histogram(position = "dodge", binwidth = 5) +
+  xlim(0,1000)+
+  ylim(0,7500)+
+  labs(title = "Ratings Distribution by Note Status, blue goes up to 50k when near 0 ratings")
+
+### Helpfulness status as a measure of publishability? ----
+ggplot(notes_merged, aes(x = not_helpful_rate, fill = current_status)) +
+  geom_histogram(position = "dodge", binwidth = 0.05) +
+  labs(title = "Disagreement Rate by Status")
+# =^
+ggplot(notes_merged, aes(x = not_helpful_rate)) +
+  geom_histogram(binwidth = 0.05) +
+  facet_wrap(~ current_status) +
+  labs(title = "Disagreement Rate by Status")
+# = ^
+ggplot(sampled_data, aes(x = helpful_rate, y = ratings, color = current_status)) +
+  geom_point() +
+  labs(title = "Helpfulness vs Ratings by Status")
+
+
+#Summary:
+# How many ratings needed to publish a note: 
+#unknown, we need to get information on notes actually 
+#being published, there seems to be no clear indication of 
+#how many rates/agreement rate but at some point they are 
+#definitely published because most published notes have more 
+#ratings and higher agreement.
+#
+# How many people need to disagree: 
+#similar procedure to first question, we need to see the 
+#data about published notes and then use the not_helpful_rate 
+#to determine publishability.
+
+# Disagreement defined as a high not helpful rate 
+#(mostly) or a low ratings score
+
+# Agreement rate, helpful rate, and ratings are key to 
+#getting a note published, need to investigate note_length 
+#as well as trustworthy sources to see if there is correlation.
